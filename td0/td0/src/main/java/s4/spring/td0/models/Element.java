9ff6adcc07d45d1f1df2cbd944a61fa9e4d7e334
Thomas <21820757@etu.unicaen.fr>
2020-01-28 15:09:01 +0100

    ajout classe element

---
+++ td0/td0/src/main/java/s4/spring/td0/models/Element.java
@@ -0,0 +1,14 @@
+package s4.spring.td0.models;
+
+public class Element{
+    private String nom;
+    private int evaluation;
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public void setNom(String nom){
+        this.nom=nom; ;
+    }
+}
