add26e0ae3908d58153dd15461e4a6cc627391b4
Thomas <21820757@etu.unicaen.fr>
2020-02-04 14:06:19 +0100

    add new project

--- td2/src/main/java/s4/spring/td2/Td2Application.java
+++ td2/src/main/java/s4/spring/td2/Td2Application.java
@@ -1,13 +1,13 @@
-package s4.spring.td0;
+package s4.spring.td2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Td0Application {
+public class Td2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Td0Application.class, args);
+		SpringApplication.run(Td2Application.class, args);
 	}
 
 }
