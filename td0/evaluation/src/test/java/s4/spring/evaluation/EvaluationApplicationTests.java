2a260aec4f3d3e70fb57a04e66df0180cfc59489
Thomas <21820757@etu.unicaen.fr>
2020-03-26 15:42:19 +0100

    first commit

---
+++ td0/evaluation/src/test/java/s4/spring/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package s4.spring.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
